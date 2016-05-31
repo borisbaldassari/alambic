30071728674ac3dafc8159a598e81e6c7f66fbad
Boris Baldassari <boris.baldassari@gmail.com>
2016-05-31 11:14:02 +0200

    #46 Improve EclipseReport plugin.

--- mojo/lib/Alambic/Plugins/EclipseReport.pm
+++ mojo/lib/Alambic/Plugins/EclipseReport.pm
@@ -70,7 +70,7 @@ sub run_post($$) {
     }
     push( @log, "[Plugins::EclipseReport] Writing main csv file to plugins and output directories." );
     $repofs->write_plugin( 'EclipseReport', $project_id . "_main.csv", $csv_out );
-    $repofs->write_output( 'EclipseReport', $project_id . "_main.csv", $csv_out );
+    $repofs->write_output( $project_id, "main.csv", $csv_out );
 
     # Write info information for the project
     $csv_out = "Mnemo,Value\n";
@@ -79,7 +79,7 @@ sub run_post($$) {
     }
     push( @log, "[Plugins::EclipseReport] Writing info csv file to plugins and output directories." );
     $repofs->write_plugin( 'EclipseReport', $project_id . "_info.csv", $csv_out );
-    $repofs->write_output( 'EclipseReport', $project_id . "_info.csv", $csv_out );
+    $repofs->write_output( $project_id, "info.csv", $csv_out );
 
     # Write metrics information for the project
     $csv_out = "Mnemo,Value\n";
@@ -88,7 +88,7 @@ sub run_post($$) {
     }
     push( @log, "[Plugins::EclipseReport] Writing metrics csv file to plugins and output directories." );
     $repofs->write_plugin( 'EclipseReport', $project_id . "_metrics.csv", $csv_out );
-    $repofs->write_output( 'EclipseReport', $project_id . "_metrics.csv", $csv_out );
+    $repofs->write_output( $project_id, "metrics.csv", $csv_out );
 
     # Write attributes information for the project
     $csv_out = "Mnemo,Value\n";
@@ -97,17 +97,17 @@ sub run_post($$) {
     }
     push( @log, "[Plugins::EclipseReport] Writing attributes csv file to plugins and output directories." );
     $repofs->write_plugin( 'EclipseReport', $project_id . "_attributes.csv", $csv_out );
-    $repofs->write_output( 'EclipseReport', $project_id . "_attributes.csv", $csv_out );
+    $repofs->write_output($project_id,  "attributes.csv", $csv_out );
 
     # Write recs information for the project
     $csv_out = "Mnemo,Severity,Description\n";
     foreach my $m (sort @{$project->{'recs'}} ) {
-	$csv_out .= $m->{'mnemo'} . "," . $m->{'severity'} 
-	. "," . $m->{'desc'} . "\n";
+	$csv_out .= $m->{'rid'} . "," . ( $m->{'severity'} || 0 )
+	. "," . ( $m->{'desc'} || '' ) . "\n";
     }
     push( @log, "[Plugins::EclipseReport] Writing recs csv file to plugins and output directories." );
     $repofs->write_plugin( 'EclipseReport', $project_id . "_recs.csv", $csv_out );
-    $repofs->write_output( 'EclipseReport', $project_id . "_recs.csv", $csv_out );
+    $repofs->write_output( $project_id, "recs.csv", $csv_out );
 
     # Now execute the main R script.
     push( @log, "[Plugins::EclipseReport] Executing R report." );
