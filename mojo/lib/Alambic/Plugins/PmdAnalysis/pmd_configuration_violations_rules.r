5ff3cd960f421b389610dcafe0443571964ef7cc
Boris Baldassari <boris.baldassari@gmail.com>
2016-06-23 09:02:30 +0200

    #46 Add PMD Configuration files.

---
+++ mojo/lib/Alambic/Plugins/PmdAnalysis/pmd_configuration_violations_rules.r
@@ -0,0 +1,25 @@
+require(ggplot2)
+
+variables <- commandArgs(trailingOnly=TRUE)
+project.id <- variables[1]
+plugin.id <- variables[2]
+
+priority.colours <- c("#CC0000", "#DD5500", "#EEAA00", "#FFCC66")
+
+pmd.violations <- read.csv(file=paste("", project.id, "_pmd_analysis_violations.csv", sep=""), header=T)
+
+svg("pmd_configuration_violations_rules.svg", width=14, height=8)
+ggplot(data=pmd.violations, aes(x=reorder(Mnemo, vol), y=vol+1, fill=factor(priority))) +
+  geom_bar(stat='identity') +
+  scale_fill_manual(values=priority.colours) +
+  scale_y_log10() +
+  theme(axis.text.x = element_text(angle=60, hjust=1, vjust=1)) +
+  xlab("") +
+  ylab('Non-conformities') +
+  geom_text(aes(label=vol), vjust=-0.2, size=3) +
+  labs(fill='Priority') +
+  theme(
+    panel.background = element_rect(fill = "transparent", colour = NA),
+    plot.background = element_rect(fill = "white", colour = NA)
+  )
+dev.off()
