9606589068ba44275c011825505ac4f29ee8c1da
Boris Baldassari <boris.baldassari@gmail.com>
2016-07-04 16:33:47 +0200

    #59 Improve/Fix MLS plugin, add MLS metrics.

--- mojo/lib/Alambic/Plugins/EclipseMls.pm
+++ mojo/lib/Alambic/Plugins/EclipseMls.pm
@@ -20,7 +20,7 @@ my %conf = (
     "type" => 'pre',
     "ability" => [ 'metrics', 'figs', 'viz' ],
     "params" => {
-        "project_grim" => "The project ID used to identify the project on the dashboard server.",
+        "project_grim" => "The ID used to identify the project on the dashboard server.",
     },
     "provides_cdata" => [
     ],
