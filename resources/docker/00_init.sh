30e0c6227bfa147b6e81e6fec889a29dc13a4b52
Boris Baldassari <boris.baldassari@gmail.com>
2016-12-17 11:50:04 +0100

    #81 update codeship files for ci.

--- resources/docker/00_init.sh
+++ resources/docker/00_init.sh
@@ -9,12 +9,12 @@ su - alambic -c 'perlbrew install-cpanm'
 su - alambic -c 'perlbrew --notest install perl-5.22.0'
 
 su - alambic -c 'perlbrew switch perl-5.22.0'
-su - alambic -c 'cpanm Test::More inc::latest Net::IDN::Encode Mojolicious XML::LibXML List::MoreUtils IO::Socket::SSL Minion Mojolicious::Plugin::Mail DateTime Date::Parse File::chdir'
 
-echo 1
+su - alambic -c 'cpanm Test::More inc::latest Net::IDN::Encode Mojolicious XML::LibXML List::MoreUtils IO::Socket::SSL Minion Mojolicious::Plugin::Mail DateTime Date::Parse File::chdir Text::CSV Crypt::PBKDF2'
 
-#su - alambic -c 'cpanm  Mojo::Pg' + postgresql95-devel
-#su - alambic -c 'git clone https://bitbucket.org/BorisBaldassari/alambic.git'
+su - alambic -c 'POSTGRES_HOME=/usr/pgsql-9.5 cpanm  Mojo::Pg'
+
+su - alambic -c 'git clone https://bitbucket.org/BorisBaldassari/alambic.git'
 
 
 
