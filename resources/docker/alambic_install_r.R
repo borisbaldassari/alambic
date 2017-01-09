9e34e94d911ffcffcc227edea4bfca52a76818ca
Boris Baldassari <boris.baldassari@gmail.com>
2017-01-09 08:01:33 +0100

    #81 Update R install to docker file.

--- resources/docker/alambic_install_r.R
+++ resources/docker/alambic_install_r.R
@@ -2,5 +2,3 @@
 
 install.packages(c("BH", "DBI", "NLP", "R6", "RColorBrewer", "Rcpp", "SnowballC", "assertthat", "backports", "base64enc", "bitops", "caTools", "colorspace", "curl", "dichromat", "digest", "dplyr", "dygraphs", "evaluate", "ggplot2", "ggthemes", "googleVis", "gtable", "hexbin", "highr", "htmltools", "htmlwidgets", "httr", "jsonlite", "knitr", "labeling", "lazyeval", "magrittr", "markdown", "mime", "munsell", "openssl", "packrat", "pander", "plotly", "plyr", "purrr", "reshape2", "rmarkdown", "rprojroot", "scales", "slam", "stringi", "stringr", "tibble", "tidyr", "tm", "viridisLite", "wordcloud", "xtable", "xts", "yaml", "zoo"), 
 	repos='http://cran.r-project.org')
-require('packrat')
-packrat::unbundle(bundle="mojo-2016-12-31.tar.gz", where=".")
