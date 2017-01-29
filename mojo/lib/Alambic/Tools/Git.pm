f9aa6afef194ced98c412e093ba9d27c511e38f2
Boris Baldassari <boris.baldassari@gmail.com>
2017-01-29 16:36:09 +0100

    #93 Update R with more features/minor fixes.

--- mojo/lib/Alambic/Tools/Git.pm
+++ mojo/lib/Alambic/Tools/Git.pm
@@ -4,12 +4,9 @@ use strict;
 use warnings;
 
 use Alambic::Model::RepoFS;
-use File::chdir;
-use File::Copy;
-use Try::Tiny;
 use Git::Repository;
-#use File::Path qw(remove_tree);
-    
+use Date::Parse;
+
 use Data::Dumper;
 
 # Main configuration hash for the tool
@@ -34,11 +31,13 @@ my %conf = (
 );
 
 my $git;
+my $git_url;
 my $repofs;
 
 # Constructor
 sub new {
-    my ($class) = @_;
+    my $class = shift;
+    $git_url = shift;
 
     $git = Git::Repository->new();
     $repofs = Alambic::Model::RepoFS->new();
@@ -54,6 +53,19 @@ sub get_src_path($) {
     my ($self, $project) = @_;
     
     return "projects/" . $project . "/src";
+}
+
+sub url() {
+    my ($self, $url) = @_;
+
+    my $ret;
+    if (scalar @_ > 1) {
+	$git_url = $url;
+    } else {
+	$url = $git_url;
+    }
+    
+    return $url;
 }
 
 sub install() {
@@ -95,6 +107,40 @@ sub test() {
 }
 
 
+# Function to get an pulld git repository locally, not even
+# knowing if it's already there or not. If it exists, it will be pulld.
+# If it doesn't, it is cloned.
+#
+# Params:
+#   - $plugin_id: the name/id of the calling plugin, e.g. EclipseIts.
+#   - $project_id: the id of the project analysed.
+#   - $url: the url of the git repository to clone
+sub git_clone_or_pull($$$$) {
+    my ($self, $plugin_id, $project_id, $url) = @_;
+
+    my @log;
+
+    $url = $git_url if (not defined($url));
+    my $dir = &get_src_path($self, $project_id);
+
+    if (-e $dir) {
+	# start from an existing working copy    
+	push( @log, "[Tools::Git] Directory [$dir] exists." );
+	eval {
+	    my $r = Git::Repository->new( work_tree => $dir );
+	    push( @log, "[Tools::Git] Version is " . $r->version );
+	    @log = ( @log, @{&git_pull($self, $plugin_id, $project_id)} );
+	};
+    } else {
+	# repository doesn't exist, clone src from git server.
+	push( @log, "[Tools::Git] Directory [$dir] doesn't exist. Cloning." );
+	@log = ( @log, @{&git_clone($self, $plugin_id, $project_id, $url)} );
+    }
+
+    return \@log;
+}
+
+
 # Function to knit a rmarkdown document to a html snippet.
 # It goes into the plugin's directory, 
 # creates required directories (e.g. figures/) and executes Rscript.
@@ -116,9 +162,6 @@ sub git_clone($$$$) {
     push( @log, "[Tools::Git] Cloning [$url] to [$dir]." );
     Git::Repository->run( clone => $url, $dir );
 
-    # Set vars.
-
-
     return \@log;
 }
 
@@ -135,16 +178,62 @@ sub git_clone($$$$) {
 #   - $url: the url of the git repository to clone
 #   - %params: a ref to hash of parameters/values for the execution.
 sub git_log($$$$) {
-    my ($self, $plugin_id, $project_id, $params) = @_;
+    my ($self, $plugin_id, $project_id) = @_;
     
     my @log;
 
     push( @log, "[Tools::Git] Getting Git log for [$project_id]." );
     my $output = $git->run( ( 'log' ) );
     $repofs->write_input( $project_id, "import_git.txt", $output );
-    push( @log, "[Tools::Git] Created file in input." );
+    push( @log, "[Tools::Git] Created file [${project_id}_import_git.txt] in input." );
 
     return \@log;
+}
+
+# Returns an array of commits
+sub git_commits() {
+    my ($self, $plugin_id, $project_id) = @_;
+    
+
+    my @log = $git->run( ('log', '--format=%H %at %s%n author [%aE]%n committer [%cE]', '--stat') );
+    my $log_ = _parse_git_log( @log);
+    
+    return $log_;
+}
+
+
+# Utility to parse git log
+# http://preaction.me/talks/Perl/Scripting-Git.html
+sub _parse_git_log {
+    my @lines = @_;
+    
+    my @commits;
+    my %commit;
+    my $id;
+    for my $line ( @lines ) {
+        if ( $line =~ /^(\w+) (\d+) (.*)$/ ) {
+	    $id = $1;
+	    $commit{'id'} = $1;
+	    $commit{'time'} = $2;
+	    $commit{'msg'} = $3;
+	} elsif ( $line =~ /^\s+author\s\[([^]]+)\]/ ) {
+	    $commit{'auth'} = $1;
+	} elsif ( $line =~ /^\s+committer \[([^]]+)\]/ ) {
+	    $commit{'cmtr'} = $1;
+	} elsif ( $line =~ /^\s+(\d+) files? changed(, (\d+) insert[^,]+)?(, (\d+) del[^,]+)?.*$/ ) {
+	    $commit{'mod'} = $1;
+	    $commit{'add'} = $3 if defined($3);
+	    $commit{'del'} = $5 if defined($5);
+	    my %commit_ = %commit;
+	    push( @commits, \%commit_ );
+	    undef %commit;
+	} elsif ( $line =~ /^\s*$/ ) {
+	} else { 
+	    print "Failed [$line].\n" unless ($line =~ m!\|!);
+	}
+    }
+
+    return \@commits;
 }
 
 
@@ -159,20 +248,18 @@ sub git_log($$$$) {
 #   - $project_id: the id of the project analysed.
 #   - $url: the url of the git repository to clone
 #   - %params: a ref to hash of parameters/values for the execution.
-sub git_update($$$$) {
-    my ($self, $plugin_id, $project_id, $url, $params) = @_;
+sub git_pull($$$$) {
+    my ($self, $plugin_id, $project_id) = @_;
     
     my @log;
-
-    my @output = $git->run( ( 'log' ) );
-    my $dir = &get_src_path($self, $project_id);
-
-    push( @log, "[Tools::Git] NOT IMPLEMENTED Updating [$url] to [$dir]." );
-#    Git::Repository->run( clone => $url, $dir );
-
-    # Set vars.
-
-
+    push( @log, "[Tools::Git] Pulling from origin." );
+    my @output = $git->run( ( 'pull' ) );
+    if (scalar @output == 1 && $output[0] =~ m!Already up-to-date!) {
+	push( @log, "[Tools::Git] Already up-to-date." );
+    } else {
+	push( @log, "[Tools::Git] Pull output: " . @output . "." );
+    }
+    
     return \@log;
 }
 
