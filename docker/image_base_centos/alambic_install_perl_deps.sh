2025ea6d5b442190d742f477be7692606ab94830
Boris Baldassari <boris.baldassari@gmail.com>
2017-02-07 09:39:51 +0100

    #81 Update codeship files.

--- docker/image_base_centos/alambic_install_perl_deps.sh
+++ docker/image_base_centos/alambic_install_perl_deps.sh
@@ -1,14 +1,14 @@
 
 
 # Download perlbrew and install it.
-
 \curl -L https://install.perlbrew.pl | bash
 echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~alambic/.bashrc
 
-# Using perlbrew, install cpanm, recent version of perl, and all modules
+source ~alambic/.bashrc
 
+# Using perlbrew, install cpanm, recent version of perl, and all modules
 perlbrew install-cpanm
 perlbrew --notest install perl-5.24.1
 perlbrew switch perl-5.24.1
 
-cpanm Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Utils List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL
+cpanm Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL
