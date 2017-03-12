876bb1bffff969fca205b24bc9d80ee40a084a4b
Boris Baldassari <boris.baldassari@gmail.com>
2017-03-12 11:10:37 +0100

    #81 Fix codefresh ci + test.

--- docker/image_ci/alambic_init_start.sh
+++ docker/image_ci/alambic_init_start.sh
@@ -15,7 +15,7 @@ cd /home/alambic/alambic/mojo
 cat alambic.conf
 
 # Initialise the app: create tables, init instance.
-script/alambic alambic init
+bin/alambic alambic init
 
 # Start main process for mojo
 hypnotoad bin/alambic
