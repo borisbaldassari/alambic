70d891abae78b10b447aed3bbd41685c5d33f407
Boris Baldassari <boris.baldassari@gmail.com>
2017-03-26 10:10:05 +0200

    #106 Tidy Perl files, add tidy script in releng.

--- resources/scripts/releng/exec_perltidy.sh
+++ resources/scripts/releng/exec_perltidy.sh
@@ -2,7 +2,7 @@
 
 IS_MOJO=$(basename `pwd`)
 if [ $IS_MOJO = "mojo" ]; then
-    for f in `find lib/ -name "*.pm"`; do perltidy ${f} > ${f}_; done
+    for f in `find lib/ -name "*.pm"`; do perltidy -pro=../resources/scripts/releng/perltidyrc ${f} > ${f}_; done
     for f in `find lib/ -name "*.pm"`; do mv ${f}_ ${f}; done
     echo "Tidied all files in lib/."
 else
