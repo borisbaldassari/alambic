d6fc170933383a79810aea82cf29021deb0902f2
Boris Baldassari <boris.baldassari@gmail.com>
2017-08-05 17:32:59 +0200

    #114 Update perl mod list in alambic_install.sh.

--- resources/scripts/alambic_check_install.sh
+++ resources/scripts/alambic_check_install.sh
@@ -105,7 +105,7 @@ perlbrew use perl-$PB_V
 echo "# Installing perl modules.."
 echo "# Installing perl modules.." >> $LOG
 POSTGRES_HOME=/usr/pgsql-9.5
-cpanm Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST >> $LOG
+cpanm Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST Mojolicious::Plugin::InstallablePaths Pod::ProjectDocs >> $LOG
 if [ $? -eq 0 ]; then
     echo "# Perl modules installed"
     echo "# Perl modules installed" >> $LOG
