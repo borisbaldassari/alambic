44aa19f46573cfe63e2ce26d5663b1f75958fdd6
Boris Baldassari <boris.baldassari@gmail.com>
2017-08-06 10:59:09 +0200

    #114 Use perl 5.26, change script path in docker file.

--- docker/image_base_centos/alambic_install_perl_deps.sh
+++ docker/image_base_centos/alambic_install_perl_deps.sh
@@ -7,8 +7,8 @@ echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~alambic/.bashrc
 source ~alambic/.bashrc
 
 # Using perlbrew, install cpanm, recent version of perl, and all modules
-#perlbrew install-cpanm
-#perlbrew --notest install perl-5.26.0
+perlbrew install-cpanm
+perlbrew --notest install perl-5.26.0
 perlbrew switch perl-5.26.0
 
 POSTGRES_HOME=/usr/pgsql-9.5 cpanm inc::Module::Install Digest::MD5 Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST Mojolicious::Plugin::InstallablePaths Pod::ProjectDocs
