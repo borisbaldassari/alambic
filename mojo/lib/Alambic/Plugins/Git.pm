950d3fdaed52d346b6a452b199eec45e870065d5
Boris Baldassari <boris.baldassari@gmail.com>
2017-08-22 13:27:21 +0200

    #125 Add Thales IP to Git code.

--- mojo/lib/Alambic/Plugins/Git.pm
+++ mojo/lib/Alambic/Plugins/Git.pm
@@ -1,6 +1,6 @@
 #########################################################
 #
-# Copyright (c) 2015-2017 Castalia Solutions and others.
+# Copyright (c) 2015-2017 Castalia Solutions and Thales Group.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
