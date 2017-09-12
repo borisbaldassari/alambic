706d56d83f5143c8a3e1c0a9c7fe077d2796f584
Boris Baldassari <boris.baldassari@gmail.com>
2017-09-12 08:50:34 +0200

    #129 Improve R markdown doc for genericR.

--- mojo/lib/Alambic/Plugins/GenericR.pm
+++ mojo/lib/Alambic/Plugins/GenericR.pm
@@ -66,43 +66,18 @@ sub run_post($$) {
   my @log;
   push(@log, "[Plugins::GenericR] Start Generic R plugin execution.");
   
-  my $repofs  = Alambic::Model::RepoFS->new();
-  my $models  = $conf->{'models'};
-  my $project = $conf->{'project'};
-  my $qm      = $project->get_qm(
-      $models->get_qm(),
-      $models->get_attributes(),
-      $models->get_metrics(),
-  );
-  my $run = $conf->{'last_run'};
+  # my $repofs  = Alambic::Model::RepoFS->new();
+  # my $models  = $conf->{'models'};
+  # my $project = $conf->{'project'};
+  # my $qm      = $project->get_qm(
+  #     $models->get_qm(),
+  #     $models->get_attributes(),
+  #     $models->get_metrics(),
+  # );
+  # my $run = $conf->{'last_run'};
 
   my $params = {};
 
-  # Create a CSV file with all metrics + values
-  my $metrics = $project->metrics();
-  my $csv = Text::CSV->new({binary => 1, eol => "\n"});
-  $csv->combine( ('Mnemo', 'Value') );
-  my $csv_out = $csv->string();
-  foreach my $metric (sort keys %$metrics) {
-      $csv->combine( ($metric, $metrics->{$metric}) );
-      $csv_out .= $csv->string();
-  }
-  
-  # Write csv file to disk.
-  $repofs->write_plugin('GenericR', $project_id . "_metrics.csv", $csv_out);
-
-  my $attributes = $project->attributes();
-  $csv = Text::CSV->new({binary => 1, eol => "\n"});
-  $csv->combine( ('Mnemo', 'Value') );
-  $csv_out = $csv->string();
-  foreach my $attribute (sort keys %$attributes) {
-      $csv->combine( ($attribute, $attributes->{$attribute}) );
-      $csv_out .= $csv->string();
-  }
-  
-  # Write csv file to disk.
-  $repofs->write_plugin('GenericR', $project_id . "_attributes.csv", $csv_out);
-  
   # Execute the figures R scripts.
   my $r = Alambic::Tools::R->new();
   push(@log, "[Plugins::GenericR] Executing R pdf markdown document.");
