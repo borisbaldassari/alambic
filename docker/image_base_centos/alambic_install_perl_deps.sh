f015b2e0ec9b6c3409f3fa35b6c19d2ff29876cd
Boris Baldassari <boris.baldassari@gmail.com>
2017-12-09 10:09:24 +0100

    #133 Add perl requirements for gitlab and minion::admin.

--- docker/image_base_centos/alambic_install_perl_deps.sh
+++ docker/image_base_centos/alambic_install_perl_deps.sh
@@ -11,7 +11,7 @@ perlbrew install-cpanm
 perlbrew --notest install perl-5.26.1
 perlbrew switch perl-5.26.1
 
-POSTGRES_HOME=/usr/pgsql-9.5 cpanm Sub::Identify DBI DBD::Pg inc::Module::Install Digest::MD5 Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST Mojolicious::Plugin::InstallablePaths Pod::ProjectDocs GitLab::API:v3 Moose HTML::Entities Template
+POSTGRES_HOME=/usr/pgsql-9.5 cpanm Sub::Identify DBI DBD::Pg inc::Module::Install Digest::MD5 Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST Mojolicious::Plugin::InstallablePaths Pod::ProjectDocs GitLab::API:v3 Moose HTML::Entities Template Mojolicious::Plugin::Minion::Admin URI::Escape::XS 
 
 
 
