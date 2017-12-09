384a49a7dfca939835d6f4b1c75642c7b81cc5ca
Boris Baldassari <boris.baldassari@gmail.com>
2017-12-09 11:43:23 +0100

    #133 Fix docker issue with base_centos.

--- docker/image_base_centos/alambic_install_perl_deps.sh
+++ docker/image_base_centos/alambic_install_perl_deps.sh
@@ -1,5 +1,4 @@
 
-
 # Download perlbrew and install it.
 \curl -L https://install.perlbrew.pl | bash
 echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~alambic/.bashrc
@@ -11,7 +10,7 @@ perlbrew install-cpanm
 perlbrew --notest install perl-5.26.1
 perlbrew switch perl-5.26.1
 
-POSTGRES_HOME=/usr/pgsql-9.5 cpanm Sub::Identify DBI DBD::Pg inc::Module::Install Digest::MD5 Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST Mojolicious::Plugin::InstallablePaths Pod::ProjectDocs GitLab::API:v3 Moose HTML::Entities Template Mojolicious::Plugin::Minion::Admin URI::Escape::XS 
+POSTGRES_HOME=/usr/pgsql-9.5 cpanm Sub::Identify DBI DBD::Pg inc::Module::Install Digest::MD5 Crypt::PBKDF2 Date::Parse DateTime File::chdir File::Basename File::Copy File::Path File::stat List::Util List::MoreUtils Minion Mojolicious Mojo::JSON Mojo::UserAgent Mojo::Pg XML::LibXML Text::CSV Time::localtime Mojolicious::Plugin::Mail Test::More Test::Perl::Critic Net::IDN::Encode IO::Socket::SSL Git::Repository JIRA::REST Mojolicious::Plugin::InstallablePaths Pod::ProjectDocs GitLab::API::v3 Moose HTML::Entities Template Mojolicious::Plugin::Minion::Admin URI::Escape::XS 
 
 
 
