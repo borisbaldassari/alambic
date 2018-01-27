59671c4e2dd1ab824afb5ed234c13f87cd7b821e
Boris Baldassari <boris.baldassari@gmail.com>
2018-01-27 10:01:09 +0100

    #147 Add Bugzilla module and tests.

---
+++ mojo/lib/Alambic/Plugins/Bugzilla.pm
@@ -0,0 +1,452 @@
+#########################################################
+#
+# Copyright (c) 2015-2017 Castalia Solutions and Thales Group.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Boris Baldassari - Castalia Solutions
+#
+#########################################################
+
+package Alambic::Plugins::Bugzilla;
+
+use strict;
+use warnings;
+
+use Alambic::Model::RepoFS;
+use Alambic::Tools::R;
+
+use Mojo::UserAgent;
+use Mojo::JSON qw( decode_json encode_json );
+use Date::Parse;
+use Time::Piece;
+use Time::Seconds;
+use Text::CSV;
+use Data::Dumper;
+
+# Main configuration hash for the plugin
+my %conf = (
+  "id"   => "Bugzilla",
+  "name" => "Bugzilla",
+  "desc" => [
+    'The Bugzilla plugin retrieves issues information from a Bugzilla server (version 5.1+), using the <a href="http://bugzilla.readthedocs.io/en/latest/api/index.html">REST API</a>.',
+    'See <a href="https://alambic.io/Plugins/Pre/Bugzilla">the project\'s wiki</a> for more information.',
+  ],
+  "type"    => "pre",
+  "ability" => ['info', 'metrics', 'data', 'figs', 'recs', 'viz', 'users'],
+  "params"  => {
+    "bugzilla_url"     => "The URL of the Bugzilla server, e.g. https://bugs.eclipse.org/bugs/.",
+    "bugzilla_project" => "The project ID to be requested on the bugzilla server.",
+    "proxy" =>
+      'If a proxy is required to access the remote resource of this plugin, please provide its URL here. A blank field means no proxy, and the <code>default</code> keyword uses the proxy from environment variables, see <a href="https://alambic.io/Documentation/Admin/Projects.html">the online documentation about proxies</a> for more details. Example: <code>https://user:pass@proxy.mycorp:3777</code>.',
+  },
+  "provides_cdata" => [],
+  "provides_info"  => ["BZ_URL",],
+  "provides_data"  => {
+    "import_bugzilla.json" =>
+      "The original file of current information, downloaded from the Bugzilla server (JSON).",
+    "bugzilla_evol.csv" =>
+      "The evolution of issues created and authors by day (CSV).",
+    "bugzilla_issues.csv" =>
+      "The list of issues, with fields 'id, summary, status, assignee, reporter, due_date, created_at, updated_at' (CSV).",
+    # "bugzilla_issues_late.csv" =>
+    #   "The list of late issues (i.e. their due_date has past), with fields 'id, summary, status, assignee, reporter, due_date, created_at, updated_at' (CSV).",
+    "bugzilla_issues_open.csv" =>
+      "The list of open issues, with fields 'id, summary, status, assignee, reporter, due_date, created_at, updated_at' (CSV).",
+    "bugzilla_issues_open_unassigned.csv" =>
+      "The list of open and unassigned issues, with fields 'id, summary, status, assignee, reporter, due_date, created_at, updated_at' (CSV).",
+  },
+  "provides_metrics" => {
+    "BZ_VOL"             => "BZ_VOL",
+    "BZ_AUTHORS"         => "BZ_AUTHORS",
+    "BZ_AUTHORS_1W"      => "BZ_AUTHORS_1W",
+    "BZ_AUTHORS_1M"      => "BZ_AUTHORS_1M",
+    "BZ_AUTHORS_1Y"      => "BZ_AUTHORS_1Y",
+    "BZ_CREATED_1W"      => "BZ_CREATED_1W",
+    "BZ_CREATED_1M"      => "BZ_CREATED_1M",
+    "BZ_CREATED_1Y"      => "BZ_CREATED_1Y",
+    "BZ_UPDATED_1W"      => "BZ_UPDATED_1W",
+    "BZ_UPDATED_1M"      => "BZ_UPDATED_1M",
+    "BZ_UPDATED_1Y"      => "BZ_UPDATED_1Y",
+    "BZ_OPEN"            => "BZ_OPEN",
+    "BZ_OPEN_PERCENT"    => "BZ_OPEN_PERCENT",
+#    "BZ_LATE"            => "BZ_LATE",
+    "BZ_OPEN_UNASSIGNED" => "BZ_OPEN_UNASSIGNED",
+  },
+  "provides_figs" => {
+    'bugzilla_summary.html' => "HTML summary of Bugzilla issues main metrics (HTML)",
+    'bugzilla_evol_summary.html' => "Evolution of Bugzilla main metrics (HTML)",
+    'bugzilla_evol_created.html' => "Evolution of Bugzilla issues creation (HTML)",
+    'bugzilla_evol_authors.html' => "Evolution of Bugzilla issues authors (HTML)",
+  },
+  "provides_recs" => ["BZ_LATE_ISSUES",],
+  "provides_viz"  => {"bugzilla.html" => "Bugzilla",},
+);
+
+# Constructor
+sub new {
+  my ($class) = @_;
+
+  return bless {}, $class;
+}
+
+
+sub get_conf() {
+  return \%conf;
+}
+
+
+# Run plugin: retrieves data + compute_data
+sub run_plugin($$) {
+  my ($self, $project_id, $conf) = @_;
+
+  my %ret = ('metrics' => {}, 'info' => {}, 'recs' => [], 'log' => [],);
+
+  # Create RepoFS object for writing and reading files on FS.
+  my $repofs = Alambic::Model::RepoFS->new();
+
+  # Time::Piece object. Will be used for the date calculations.
+  my $t_now = localtime;
+  my $t_1w  = $t_now - ONE_WEEK;
+  my $t_1m  = $t_now - ONE_MONTH;
+  my $t_1y  = $t_now - ONE_YEAR;
+
+  my $bugzilla_url         = $conf->{'bugzilla_url'};
+  my $bugzilla_project = $conf->{'bugzilla_project'};
+  my $proxy_url    = $conf->{'proxy'} || '';
+
+  # Set info BZ_URL
+  my $url_base = "https://bugs.eclipse.org/bugs/";
+  $ret{'info'}{'BZ_URL'} = $url_base . '/buglist.cgi?product=' . $bugzilla_project;
+
+#  my $bugzilla_conf
+#    = {url => $bugzilla_url, username => $bugzilla_user, password => $bugzilla_passwd,};
+
+  # Configure Proxy
+  if ($proxy_url =~ m!^default!i) {
+
+    # If 'default', then use detect
+    foreach my $v (('https_proxy', 'HTTPS_PROXY', 'http_proxy', 'HTTP_PROXY')) {
+      if (defined($ENV{$v})) {
+#        $bugzilla_conf->{'proxy'} = $ENV{$v};
+        push(
+          @{$ret{'log'}},
+          "[Plugins::Bugzilla] Using default proxy ["
+#            . $bugzilla_conf->{'proxy'} . "]."
+        );
+        last;
+      }
+    }
+  }
+  elsif ($proxy_url =~ m!\S+$!) {
+    # If something, then use it
+#    $bugzilla_conf->{'proxy'} = $proxy_url;
+    push(
+      @{$ret{'log'}},
+      "[Plugins::Bugzilla] Using provided proxy [$proxy_url]."
+    );
+  }
+  else {
+    # If blank, then use no proxy
+    push(@{$ret{'log'}}, "[Plugins::Bugzilla] No proxy defined [$proxy_url].");
+  }
+
+
+  my $ua  = Mojo::UserAgent->new;
+  my @bugs;
+
+  # Define the list of attributes we want to use for the list of bugs
+  # This is only used when no bug_id is provided, though.
+  my @attrs_def = ('id', 'summary', 'status', 'resolution', 'severity', 'priority', 'classification', 
+             'platform', 'product', 'version', 'component', 'creation_time', 'creator', 
+             'assigned_to', 'last_change_time', 'target_milestone', 'url');
+
+  my $url = $url_base . "rest/bug?product=" . $bugzilla_project . 
+      "&include_fields=" . join(',', @attrs_def);
+  
+  push(@{$ret{'log'}}, "[Plugins::Bugzilla] Using URL [$url].");
+  my $res = $ua->get($url)->result;
+  
+  if (not $res->is_success) {
+      push(@{$ret{'log'}}, 
+           "[Plugins::Bugzilla] ERROR: Could not get resource [$url].\n" . 
+           "Message is: " . $res->message . "\n" );
+      return \%ret;
+  }
+  
+  my $json = $res->body;
+  my $data = decode_json($json); 
+  push(@{$ret{'log'}}, "[Plugins::Bugzilla] Found " . scalar(@{$data->{'bugs'}}) . " issues.\n");
+  
+  # Write json file to import directory
+  $repofs->write_input( $project_id, "import_bugzilla.json",
+                        encode_json($data->{'bugs'}) );
+
+  # my (@late, @open, @unassigned_open, %people);
+  my $csv_out = join( ',', @attrs_def );
+
+  my $csv_open_out            = $csv_out;
+  my $csv_unassigned_open_out = $csv_out;
+  
+  my ($bz_created_1w, $bz_created_1m, $bz_created_1y) = (0, 0, 0);
+  my ($bz_updated_1w, $bz_updated_1m, $bz_updated_1y) = (0, 0, 0);
+  my (%authors, %authors_1w, %authors_1m, %authors_1y, %people);
+  my (@open, @unassigned_open);
+  my %timeline_c;
+  my %timeline_a;
+  
+  my $csv                 = Text::CSV->new({binary => 1, eol => "\n"});
+#  my $csv_late            = Text::CSV->new({binary => 1, eol => "\n"});
+  my $csv_open            = Text::CSV->new({binary => 1, eol => "\n"});
+  my $csv_unassigned_open = Text::CSV->new({binary => 1, eol => "\n"});
+
+  foreach my $issue (@{$data->{'bugs'}}) {
+
+      # Convert string dates to epoch seconds      
+      my $date_created
+          = Time::Piece->strptime(int(str2time($issue->{'creation_time'}) || 0), "%s");
+      my $date_updated
+          = Time::Piece->strptime(int(str2time($issue->{'creation_time'}) || 0), "%s");
+      my $t_now = time();
+
+      my $date_m = $date_created->strftime("%Y-%m-%d"); 
+      $timeline_c{$date_m}++;
+      $timeline_a{$date_m}{$issue->{'creator'}}++;
+      $authors{$issue->{'creator'}}++;
+
+      # Rebuild the URL for the issue.
+      my $issue->{'url'} = $url_base . '/bugs/show_bug.cgi?id=' . $issue->{'id'};
+      
+      my @attrs_v = map { $issue->{$_} } @attrs_def;
+      $csv->combine(@attrs_v);
+      $csv_out .= $csv->string();
+      
+      if ( $issue->{'is_open'} ) {
+          push(@open, $issue->{'id'});
+          $csv_open_out .= $csv->string();
+      }
+
+      #   # Check if issue's due date has past
+      #   if (defined($date_due) and $date_due < $t_now) {
+      #     push(@late, $issue->{'key'});
+      #     $csv_late_out .= $csv->string();
+      #   }
+      
+      # Check if issue is assigned and open
+      if ( (not defined($issue->{'assigned_to'}))
+           && grep($issue->{'is_open'}) ) {
+          push(@unassigned_open, $issue->{'id'});
+          $csv_unassigned_open_out .= $csv->string();
+      }
+      
+      # Populate %people to show activity in the user's profile
+      if ( exists $issue->{'assigned_to'} ) {
+          my $event = {
+              "type" => "issue_assigned",
+              "id"   => $issue->{'id'},
+              "msg"  => $issue->{'summary'},
+              "url"  => $issue->{'url'},
+          };
+          push(@{$people{$issue->{'assigned_to'}}}, $event);
+      }
+      
+      # Populate %people to show activity in the user's profile
+      if (exists $issue->{'creator'}) {
+          my $event = {
+              "type" => "issue_created",
+              "id"   => $issue->{'id'},
+              "msg"  => $issue->{'summary'},
+              "url"  => $issue->{'url'},
+              "time" => $issue->{'creation_time'}
+          };
+          push(@{$people{$issue->{'creator'}}}, $event);
+      }
+      
+      # Is the issue recent (<1W)?
+      if ($date_created > $t_1w->epoch) {
+          $authors_1w{$issue->{'creator'}}++;
+          $bz_created_1w++;
+      }
+      
+      # Is the issue recent (<1M)?
+      if ($date_created > $t_1m->epoch) {
+          $authors_1m{$issue->{'creator'}}++;
+          $bz_created_1m++;
+      }
+      
+      # Is the issue recent (<1Y)?
+      if ($date_created > $t_1y->epoch) {
+          $authors_1y{$issue->{'creator'}}++;
+          $bz_created_1y++;
+      }
+      
+      # Has the issue been updated recently (<1W)?
+      if ($date_updated > $t_1w->epoch) {
+          $bz_updated_1w++;
+      }
+      
+      # Has the issue been updated recently (<1M)?
+      if ($date_updated > $t_1m->epoch) {
+          $bz_updated_1m++;
+      }
+      
+      # Has the issue been updated recently (<1Y)?
+      if ($date_updated > $t_1y->epoch) {
+          $bz_updated_1y++;
+      }
+  }
+  
+  # Write metrics to csv and json files.
+  $repofs->write_output($project_id, "bugzilla_issues.csv",      $csv_out);
+  # $repofs->write_output($project_id, "bugzilla_issues_late.csv", $csv_late_out);
+  $repofs->write_output($project_id, "bugzilla_issues_open.csv", $csv_open_out);
+  $repofs->write_output($project_id, "bugzilla_issues_open_unassigned.csv",
+                        $csv_unassigned_open_out);
+
+  # Compute and store metrics
+  $ret{'metrics'}{'BZ_VOL'}     = scalar @{$data->{'bugs'}};
+  $ret{'metrics'}{'BZ_AUTHORS'} = scalar keys %authors;
+  $ret{'metrics'}{'BZ_OPEN'}    = scalar @open;
+  $ret{'metrics'}{'BZ_OPEN_PERCENT'}
+  = sprintf("%.0f", 100 * (scalar @open) / (scalar @{$data->{'bugs'}}));
+  # $ret{'metrics'}{'BZ_LATE'}            = scalar @late            || 0;
+  $ret{'metrics'}{'BZ_OPEN_UNASSIGNED'} = scalar @unassigned_open || 0;
+  $ret{'metrics'}{'BZ_AUTHORS_1W'}      = scalar keys %authors_1w || 0;
+  $ret{'metrics'}{'BZ_AUTHORS_1M'}      = scalar keys %authors_1m || 0;
+  $ret{'metrics'}{'BZ_AUTHORS_1Y'}      = scalar keys %authors_1y || 0;
+  $ret{'metrics'}{'BZ_CREATED_1W'}      = $bz_created_1w;
+  $ret{'metrics'}{'BZ_CREATED_1M'}      = $bz_created_1m;
+  $ret{'metrics'}{'BZ_CREATED_1Y'}      = $bz_created_1y;
+  $ret{'metrics'}{'BZ_UPDATED_1W'}      = $bz_updated_1w;
+  $ret{'metrics'}{'BZ_UPDATED_1M'}      = $bz_updated_1m;
+  $ret{'metrics'}{'BZ_UPDATED_1Y'}      = $bz_updated_1y;
+  
+  # Set user information for profile
+  push(@{$ret{'log'}}, "[Plugins::Bugzilla] Writing user events file.");
+  my $events = {};
+  foreach my $u (sort keys %people) {
+      $events->{$u} = $people{$u};
+  }
+  $repofs->write_users("Bugzilla", $project_id, $events);
+  
+  # Write bugzilla metrics json file to disk.
+  $repofs->write_output($project_id, "metrics_bugzilla.json",
+                        encode_json($ret{'metrics'}));
+
+  # Write bugzilla metrics csv file
+  my @metrics = map { $conf{'provides_metrics'}{$_} } sort keys %{$conf{'provides_metrics'}};
+  $csv_out = join( ',', @metrics ) . "\n";
+  my @attrs_v = map { $ret{'metrics'}{$_} } @metrics;
+  $csv->combine(@attrs_v);
+  $csv_out .= $csv->string();
+  $repofs->write_output($project_id, "metrics_bugzilla.csv", $csv_out);
+
+  # Write issues history csv file to disk.
+  my %timeline = (%timeline_a, %timeline_c);
+  my @timeline
+      = map { $_ . "," . $timeline_c{$_} . "," . scalar(keys %{$timeline_a{$_}}) }
+     sort keys %timeline;
+
+  $csv_out = "date,issues_created,authors\n";
+  $csv_out .= join("\n", @timeline) . "\n";
+  $repofs->write_output($project_id, "bugzilla_evol.csv", $csv_out);
+
+  # # Now execute the main R script.
+  # push(@{$ret{'log'}}, "[Plugins::Bugzilla] Executing R main file.");
+  # my $r = Alambic::Tools::R->new();
+  # @{$ret{'log'}} = (
+  #   @{$ret{'log'}},
+  #   @{$r->knit_rmarkdown_inc('Bugzilla', $project_id, 'bugzilla_its.Rmd')}
+  # );
+
+  # # And execute the figures R scripts.
+  # @{$ret{'log'}} = (
+  #   @{$ret{'log'}},
+  #   @{
+  #     $r->knit_rmarkdown_html('Bugzilla', $project_id, 'bugzilla_evol_authors.rmd',
+  #       ['bugzilla_evol_authors.png', 'bugzilla_evol_authors.svg'])
+  #   }
+  # );
+  # @{$ret{'log'}} = (
+  #   @{$ret{'log'}},
+  #   @{
+  #     $r->knit_rmarkdown_html('Bugzilla', $project_id, 'bugzilla_evol_created.rmd',
+  #       ['bugzilla_evol_created.png', 'bugzilla_evol_created.svg'])
+  #   }
+  # );
+  # @{$ret{'log'}} = (
+  #   @{$ret{'log'}},
+  #   @{
+  #     $r->knit_rmarkdown_html('Bugzilla', $project_id, 'bugzilla_evol_summary.rmd',
+  #       [])
+  #   }
+  # );
+  # @{$ret{'log'}} = (
+  #   @{$ret{'log'}},
+  #   @{$r->knit_rmarkdown_html('Bugzilla', $project_id, 'bugzilla_summary.rmd', [])}
+  # );
+
+  # # Recommendation for late issues
+  # if (scalar @late) {
+  #   push(
+  #     @{$ret{'recs'}},
+  #     {
+  #       'rid'      => 'BZ_LATE_ISSUES',
+  #       'severity' => 2,
+  #       'src'      => 'JiraIts',
+  #       'desc'     => 'There are '
+  #         . scalar @late
+  #         . ' issues with a past due date. Either re-plan them or mark them done.'
+  #     }
+  #   );
+  # }
+
+  return \%ret;
+}
+
+
+1;
+
+
+=encoding utf8
+
+=head1 NAME
+
+B<Alambic::Plugins::JiraIts> - Retrieves issue tracking data and metrics 
+from a Jira server.
+
+=head1 DESCRIPTION
+
+B<Alambic::Plugins::JiraIts> Retrieves issue tracking data and metrics 
+from a Jira Server.
+
+Parameters: 
+
+=over
+
+=item * jira_open_states A list of status names considered as open in the workflow, coma-separated.
+
+=item * jira_passwd The password of the user for the connection to the Jira server.
+
+=item * jira_project The Identifier of the project within the Jira instance, e.g. ALX.
+
+=item * jira_url The URL of the Jira server, e.g. https://tracker.openattic.org.
+
+=back
+=item * jira_user The id of the user for the connection to the Jira server.
+
+
+For the complete description of the plugin see the user documentation on the web site: L<https://alambic.io/Plugins/Pre/Jira.html>.
+
+=head1 SEE ALSO
+
+L<https://alambic.io/Plugins/Pre/Jira.html>,
+
+L<Mojolicious>, L<http://alambic.io>, L<https://bitbucket.org/BorisBaldassari/alambic>, L<https://www.atlassian.com/software/jira>
+
+
+=cut
---
+++ mojo/t/unit/Plugins/Buzilla.t
@@ -0,0 +1,205 @@
+#! perl -I../../lib/
+#########################################################
+#
+# Copyright (c) 2015-2017 Castalia Solutions and Thales Group.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Boris Baldassari - Castalia Solutions
+#
+#########################################################
+
+
+use strict;
+use warnings;
+
+use Test::More;
+use Mojo::JSON qw( decode_json);
+use Data::Dumper;
+
+BEGIN { use_ok('Alambic::Plugins::Bugzilla'); }
+
+my $plugin = Alambic::Plugins::Bugzilla->new();
+isa_ok($plugin, 'Alambic::Plugins::Bugzilla');
+
+note("Checking the plugin parameters. ");
+my $conf = $plugin->get_conf();
+
+ok(grep(m!bugzilla_url!, keys %{$conf->{'params'}}), "Conf has params > bugzilla_url");
+ok(grep(m!bugzilla_project!, keys %{$conf->{'params'}}),
+  "Conf has params > bugzilla_project");
+ok(grep(m!proxy!, keys %{$conf->{'params'}}),
+  "Conf has params > proxy");
+
+ok(grep(m!info!,    @{$conf->{'ability'}}), "Conf has ability > info");
+ok(grep(m!metrics!, @{$conf->{'ability'}}), "Conf has ability > metrics");
+ok(grep(m!data!,    @{$conf->{'ability'}}), "Conf has ability > data");
+ok(grep(m!recs!,    @{$conf->{'ability'}}), "Conf has ability > recs");
+ok(grep(m!figs!,    @{$conf->{'ability'}}), "Conf has ability > figs");
+ok(grep(m!viz!,     @{$conf->{'ability'}}), "Conf has ability > viz");
+ok(grep(m!users!,   @{$conf->{'ability'}}), "Conf has ability > users");
+
+ok(grep(m!BZ_URL!, @{$conf->{'provides_info'}}),
+  "Conf has provides_info > BZ_URL");
+
+ok(grep(m!import_bugzilla.json!, keys %{$conf->{'provides_data'}}),
+  "Conf has provides_data > import_bugzilla.json");
+ok(grep(m!bugzilla_evol.csv!, keys %{$conf->{'provides_data'}}),
+  "Conf has provides_data > bugzilla_evol.csv");
+ok(grep(m!bugzilla_issues.csv!, keys %{$conf->{'provides_data'}}),
+  "Conf has provides_data > bugzilla_issues.csv");
+ok(grep(m!bugzilla_issues_open.csv!, keys %{$conf->{'provides_data'}}),
+  "Conf has provides_data > bugzilla_issues_open.csv");
+ok(grep(m!bugzilla_issues_open_unassigned.csv!, keys %{$conf->{'provides_data'}}),
+  "Conf has provides_data > bugzilla_issues_open_unassigned.csv");
+
+
+ok(grep(m!BZ_VOL!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_VOL");
+ok(grep(m!BZ_AUTHORS!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_AUTHORS");
+ok(grep(m!BZ_AUTHORS_1M!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_AUTHORS_1M");
+ok(grep(m!BZ_AUTHORS_1W!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_AUTHORS_1W");
+ok(grep(m!BZ_AUTHORS_1Y!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_AUTHORS_1Y");
+ok(grep(m!BZ_CREATED_1W!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_CREATED_1W");
+ok(grep(m!BZ_UPDATED_1W!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_UPDATED_1W");
+ok(grep(m!BZ_OPEN!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_OPEN");
+ok(grep(m!BZ_OPEN_UNASSIGNED!, keys %{$conf->{'provides_metrics'}}),
+  "Conf has provides_metrics > BZ_OPEN_UNASSIGNED");
+
+ok(grep(m!bugzilla_summary.html!, keys %{$conf->{'provides_figs'}}),
+  "Conf has provides_figs > bugzilla_summary.html");
+
+note("Executing the plugin with acceleo project. ");
+my $ret = $plugin->run_plugin(
+  "test.bugzilla",
+  {
+    'bugzilla_url'         => 'https://bugs.eclipse.org/bugs',
+    'bugzilla_project'     => 'acceleo',
+  }
+);
+
+ok(
+  grep(m!\[Plugins::Bugzilla\] Using URL \[https://bugs.eclipse.org/bugs/rest/bug!,
+    @{$ret->{'log'}}),
+  "Ret has log > using url."
+) or print Dumper( $ret->{'log'} );
+ok(grep(m!\[Plugins::Bugzilla\] Found \d+ issues.!, @{$ret->{'log'}}),
+  "Ret has log > Found xxx issues.");
+ok(grep(m!\[Plugins::Bugzilla\] Writing user events file!, @{$ret->{'log'}}),
+  "Ret has log > Writing user events file.");
+#ok(grep(/^\[Tools::R\] Exec \[Rsc.*bugzilla_its.Rmd/, @{$ret->{'log'}}) == 1,
+#  "Checking if log contains bugzilla_its.Rmd R code exec.")
+#  or diag explain $ret;
+#ok(
+#  grep(/^\[Tools::R\] Exec \[Rsc.*bugzilla_evol_authors.rmd/, @{$ret->{'log'}})
+#    == 1,
+#  "Checking if log contains bugzilla_evol_authors.rmd R code exec."
+#) or diag explain $ret;
+#ok(
+#  grep(/^\[Tools::R\] Exec \[Rsc.*bugzilla_evol_created.rmd/, @{$ret->{'log'}})
+#    == 1,
+#  "Checking if log contains bugzilla_evol_created.rmd R code exec."
+#) or diag explain $ret;
+#ok(grep(/^\[Tools::R\] Exec \[Rsc.*bugzilla_summary.rmd/, @{$ret->{'log'}}) == 1,
+#  "Checking if log contains bugzilla_summary.rmd R code exec.")
+#  or diag explain $ret;
+
+ok($ret->{'metrics'}{'BZ_VOL'} =~ /^\d+$/, "BZ_VOL is a digit.")
+  or print Dumper($ret);
+ok($ret->{'metrics'}{'BZ_OPEN'} =~ /^\d+$/, "BZ_OPEN is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_OPEN_PERCENT'} =~ /^\d\d?$/,
+  "BZ_OPEN_PERCENT is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_OPEN_UNASSIGNED'} =~ /^\d+$/,
+  "BZ_OPEN_UNASSIGNED is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_CREATED_1M'} =~ /^\d+$/,
+  "BZ_CREATED_1M is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_CREATED_1Y'} =~ /^\d+$/,
+  "BZ_CREATED_1Y is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_CREATED_1W'} =~ /^\d+$/,
+  "BZ_CREATED_1W is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_UPDATED_1M'} =~ /^\d+$/,
+  "BZ_UPDATED_1M is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_UPDATED_1W'} =~ /^\d+$/,
+  "BZ_UPDATED_1W is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_UPDATED_1Y'} =~ /^\d+$/,
+  "BZ_UPDATED_1Y is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_AUTHORS_1W'} =~ /^\d+$/,
+  "BZ_AUTHORS_1W is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_AUTHORS_1M'} =~ /^\d+$/,
+  "BZ_AUTHORS_1M is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_AUTHORS_1Y'} =~ /^\d+$/,
+  "BZ_AUTHORS_1Y is a digit.")
+  or diag explain $ret;
+ok($ret->{'metrics'}{'BZ_AUTHORS'} =~ /^\d+$/, "BZ_AUTHORS is a digit.")
+  or diag explain $ret;
+
+
+#ok(scalar(@{$ret->{'recs'}}) == 1, "Ret has 1 rec.");
+#ok($ret->{'recs'}[0]{'rid'} eq "JIRA_LATE_ISSUES",
+#  "Ret has rec > JIRA_LATE_ISSUE.");
+
+ok($ret->{'info'}{'BZ_URL'} eq 'https://bugs.eclipse.org/bugs//buglist.cgi?product=acceleo',
+  "Ret has correct info BZ_URL.") or print Dumper($ret);
+
+
+note("Check that files have been created. ");
+ok(-e "projects/test.bugzilla/input/test.bugzilla_import_bugzilla.json",
+  "Check that file import_bugzilla.json exists.");
+
+ok(-e "projects/test.bugzilla/output/test.bugzilla_metrics_bugzilla.csv",
+  "Check that file metrics_bugzilla.csv exists.");
+ok(-e "projects/test.bugzilla/output/test.bugzilla_metrics_bugzilla.json",
+  "Check that file metrics_bugzilla.json exists.");
+
+ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol.csv",
+  "Check that file bugzilla_evol.csv exists.");
+ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_issues.csv",
+  "Check that file bugzilla_issues.csv exists.");
+ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_issues_open.csv",
+  "Check that file bugzilla_issues_open.csv exists.");
+ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_issues_open_unassigned.csv",
+  "Check that file bugzilla_issues_open_unassigned.csv exists.");
+
+#ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_summary.html",
+#  "Check that file bugzilla_summary.html exists.");
+#ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_its.inc",
+#  "Check that file bugzilla_its.inc exists.");
+
+# ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol_authors.html",
+#   "Check that file bugzilla_evol_authors.html exists.");
+# ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol_authors.svg",
+#   "Check that file bugzilla_evol_authors.svg exists.");
+# ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol_authors.png",
+#   "Check that file bugzilla_evol_authors.png exists.");
+
+# ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol_created.html",
+#   "Check that file bugzilla_evol_created.html exists.");
+# ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol_created.svg",
+#   "Check that file bugzilla_evol_created.svg exists.");
+# ok(-e "projects/test.bugzilla/output/test.bugzilla_bugzilla_evol_created.png",
+#   "Check that file bugzilla_evol_created.png exists.");
+
+done_testing();
+
