c8158ff757526354def19e1b3567426a43aab0c9
Boris Baldassari <boris.baldassari@gmail.com>
2018-04-09 20:29:22 +0200

    #149 Add r/rmd files for viz.

---
+++ mojo/lib/Alambic/Plugins/EclipseForums/eclipse_forums_plot.r
@@ -0,0 +1,45 @@
+#########################################################
+#
+# Copyright (c) 2015-2017 Castalia Solutions and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Boris Baldassari - Castalia Solutions
+#
+#########################################################
+
+require(ggplot2)
+
+variables <- commandArgs(trailingOnly=TRUE)
+project.id <- variables[1]
+plugin.id <- variables[2]
+
+file.out <- paste( project.id, "_so_plot.svg", sep="")
+
+file.csv = paste(project.id, '_so.csv', sep="")
+project <- read.csv(file=file.csv, header=TRUE)
+
+binary.colours <- c("#325d88", "#d9534f") # "#325d88" green = #3e9c1a)
+
+project$c.date <- as.POSIXct(project$creation_date, origin="1970-01-01")
+
+svg(file.out, width=18, height=7)
+ggplot(project, aes(x=c.date, y=answer_count, fill=factor(!is_answered))) +
+  geom_point(aes(size=score), shape=21, colour='black', alpha=0.5) +
+  scale_size(range=c(1,30)) +
+  theme(
+    panel.background=element_blank(),
+    axis.line=element_line(colour='black')
+  ) +
+  ggtitle('Questions on StackOverflow across years') +
+  xlab("Time") +
+  ylab("Number of answers") +
+  scale_fill_manual(values=binary.colours) +
+  labs( fill="Has an accepted answer", size="Number of votes" ) +
+  guides(fill=guide_legend(override.aes=list(size=5)))
+dev.off()
+
---
+++ mojo/lib/Alambic/Plugins/EclipseForums/eclipse_forums_wordcloud.r
@@ -0,0 +1,44 @@
+#########################################################
+#
+# Copyright (c) 2015-2017 Castalia Solutions and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Boris Baldassari - Castalia Solutions
+#
+#########################################################
+
+variables <- commandArgs(trailingOnly=TRUE)
+project.id <- variables[1]
+plugin.id <- variables[2]
+
+require(tm)
+require(wordcloud)
+require(SnowballC)
+
+file.out <- paste( project.id, "_so_wc.svg", sep="")
+
+file.csv = paste("../../../../projects/", project.id, "/output/", project.id, '_eclipse_forums_posts.csv', sep="")
+posts <- read.csv(file=file.csv, header=TRUE)
+
+mysrc <- DataframeSource(as.data.frame(posts$subject))
+corpus <- VCorpus(mysrc)
+#mydict <- corpus
+
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, content_transformer(removePunctuation))
+corpus <- tm_map(corpus, content_transformer(removeNumbers))
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+a <- tm_map(corpus, stemDocument)
+#a <- sapply(a, stemCompletion_mod, dict=mydict)[[1]]
+#stemCompletion_mod(docs[[1]],dictCorpus)
+#a <- tm_map(a, content_transformer(stemCompletion))
+
+svg(file.out, width=10, height=6)
+wordcloud(a, scale=c(12,1), max.words=50, random.order=FALSE, rot.per=0.15, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
+dev.off()
