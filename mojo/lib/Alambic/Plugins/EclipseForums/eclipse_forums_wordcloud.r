43c93007e98dfc55a2dfdaeef07b466f5da1eb3c
Boris Baldassari <boris.baldassari@gmail.com>
2018-04-24 23:02:53 +0200

    #149 Improve & fix Rmd/r files.

--- mojo/lib/Alambic/Plugins/EclipseForums/eclipse_forums_wordcloud.r
+++ mojo/lib/Alambic/Plugins/EclipseForums/eclipse_forums_wordcloud.r
@@ -20,16 +20,16 @@ require(tm)
 require(wordcloud)
 require(SnowballC)
 
-file.out <- paste( project.id, "_eclipse_forums_wordcloud.svg", sep="")
+file.out.svg <- paste( project.id, "_eclipse_forums_wordcloud.svg", sep="")
+file.out.png <- paste( project.id, "_eclipse_forums_wordcloud.png", sep="")
 
 file.csv = paste("../../../../projects/", project.id, "/output/", project.id, '_eclipse_forums_posts.csv', sep="")
 posts <- read.csv(file=file.csv, header=TRUE)
 
-test <- as.vector(posts$subject)
-Encoding(test)  <- "UTF-8"
-mysrc <- VectorSource(test)
+posts.vector <- as.vector(posts$subject)
+Encoding(posts.vector)  <- "UTF-8"
+mysrc <- VectorSource(posts.vector)
 corpus <- VCorpus(mysrc)
-#mydict <- corpus
 
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, content_transformer(tolower))
@@ -37,11 +37,13 @@ corpus <- tm_map(corpus, content_transformer(removePunctuation))
 corpus <- tm_map(corpus, content_transformer(removeNumbers))
 corpus <- tm_map(corpus, removeWords, stopwords("english"))
 a <- tm_map(corpus, stemDocument)
-#a <- sapply(a, stemCompletion_mod, dict=mydict)[[1]]
-#stemCompletion_mod(docs[[1]],dictCorpus)
-#a <- tm_map(a, content_transformer(stemCompletion))
 
-svg(file.out, width=10, height=6)
+svg(file.out.svg, width=10, height=6)
+wordcloud(a, scale=c(12,1), max.words=50, random.order=FALSE, rot.per=0.15, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
+dev.off()
+
+
+png(file.out.png, width=800, height=600)
 wordcloud(a, scale=c(12,1), max.words=50, random.order=FALSE, rot.per=0.15, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
 dev.off()
 
